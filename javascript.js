837573a5c9183945ca7898c83d335fd438111267
David Martens <david.martens@purchase.edu>
2019-03-06 13:36:23 -0500

    A conversation
    Updated

---
+++ javascript.js
@@ -0,0 +1,36 @@
+// WOW SCRIPT - CSS ANIMATIONS //
+
+(function ($) {
+  "use strict";
+  var wow = new WOW({
+    boxClass: "js_wow",
+    animateClass: "animate animate_duration",
+    offset: 0,
+    mobile: true,
+    live: false
+  });
+
+
+
+
+// FULLPAGE SCRIPT - FULL SCREEN WHEN SCROLLING //
+
+  $ (".js_scroll").fullpage({
+    sectionSelector: ".js_scroll",
+    scrollBar: true,
+    scrollingSpeed: 1e3,
+    afterRender: function() {
+      wow.init()
+    }
+  });
+
+
+// JQUERY //
+
+  $(window).scroll(function() {
+    var $window = $(window),
+      $body = $(".js_body"),
+    varscroll = $window.scrollTop() + $window.height() / 3;
+  })
+})
+(jQuery);
